f8b47aaf4a0219098a2b6e8f4b25529509b0ec5d
DevRafaDeSousa20 <rafaelasousa1718@gmail.com>
2026-01-04 00:49:46 -0300

    Alteração

--- src/main.py
+++ src/main.py
@@ -17,5 +17,65 @@
 # - Estruturas de decisão (if, else, elif);
 # - Impressão de dados na tela (função print).
 
+# 14 Heróis/Heróinas escolhidos para testar o código:
+# 1. Nome: Batman, XP: 950
+# 2. Nome: Mulher Maravilha, XP: 1.500
+# 3. Nome: Homem de Ferro, XP: 2.500
+# 4. Nome: Capitão América, XP: 6.500
+# 5. Nome: Thor, XP: 7.500
+# 6. Nome: Hulk, XP: 8.500
+# 7. Nome: Flash, XP: 9.500
+# 8. Nome: Lanterna Verde, XP: 10.500
+# 9. Nome: Aquaman, XP: 3.200
+# 10. Nome: Arqueiro Verde, XP: 4.800
+# 11. Nome: Ciborgue, XP: 5.500
+# 12. Nome: Viúva Negra, XP: 11.000
+# 13. Nome: Doutor Estranho, XP: 7500
+# 14. Nome: Pantera Negra, XP: 2.200 
+
 # Mensagem de Saída:
 # "O herói de nome {nome} possui {xp} de XP e está classificado como nível {nível}."
+
+# Declarando o nome dos heróis nas váriaveis e o XP correspondente
+herois = [
+    ("Batman", 950),
+    ("Mulher Maravilha", 1500),
+    ("Homem de Ferro", 2500),
+    ("Capitão América", 6500),
+    ("Thor", 7500),
+    ("Hulk", 8500),
+    ("Flash", 9500),
+    ("Lanterna Verde", 10500),
+    ("Aquaman", 3200),
+    ("Arqueiro Verde", 4800),
+    ("Ciborgue", 5500),
+    ("Viúva Negra", 11000),
+    ("Doutor Estranho", 7500),
+    ("Pantera Negra", 2200)
+] 
+
+# Função para classificar o herói com base no XP
+def classificar_heroi(nome, xp):
+    if xp <= 1000:
+        nivel = "Ferro"
+    elif xp <= 2000:
+        nivel = "Bronze"
+    elif xp <= 5000:
+        nivel = "Prata"
+    elif xp <= 7000:
+        nivel = "Ouro"
+    elif xp <= 8000:
+        nivel = "Platina"
+    elif xp <= 9000:
+        nivel = "Ascendente"
+    elif xp <= 10000:
+        nivel = "Imortal"
+    else:
+        nivel = "Radiante"
+    
+ # Imprimindo na tela de saída o nome e o nível classificatório de cada herói/heróina
+    print(f"O(a) herói(na) de nome {nome} possui {xp} de XP e está classificado como nível {nivel}.") 
+
+# Percorrendo a lista de heróis e chamando a função para cada um
+for nome, xp in herois:
+    classificar_heroi(nome, xp)
